db43d97cbf7533af6c0994be3c763912543d49c1
Jiri Kosina <jkosina@suse.cz>
2012-10-15 23:43:29 +0200

    random: fix debug format strings
    ljc: mainly to simplify patch fetching
    
    Fix the following warnings in formatting debug output:
    
    drivers/char/random.c: In function ‘xfer_secondary_pool’:
    drivers/char/random.c:827: warning: format ‘%d’ expects type ‘int’, but argument 7 has type ‘size_t’
    drivers/char/random.c: In function ‘account’:
    drivers/char/random.c:859: warning: format ‘%d’ expects type ‘int’, but argument 5 has type ‘size_t’
    drivers/char/random.c:881: warning: format ‘%d’ expects type ‘int’, but argument 5 has type ‘size_t’
    drivers/char/random.c: In function ‘random_read’:
    drivers/char/random.c:1141: warning: format ‘%d’ expects type ‘int’, but argument 5 has type ‘ssize_t’
    drivers/char/random.c:1145: warning: format ‘%d’ expects type ‘int’, but argument 5 has type ‘ssize_t’
    drivers/char/random.c:1145: warning: format ‘%d’ expects type ‘int’, but argument 6 has type ‘long unsigned int’
    
    by using '%zd' instead of '%d' to properly denote ssize_t/size_t conversion.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    
    Conflicts:
    
    	drivers/char/random.c
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -830,7 +830,7 @@ static void xfer_secondary_pool(struct entropy_store *r, size_t nbytes)
 		bytes = min_t(int, bytes, sizeof(tmp));
 
 		DEBUG_ENT("going to reseed %s with %d bits "
-			  "(%d of %d requested)\n",
+			  "(%zu of %d requested)\n",
 			  r->name, bytes * 8, nbytes * 8, r->entropy_count);
 
 		bytes = extract_entropy(r->pull, tmp, bytes,
@@ -862,7 +862,7 @@ static size_t account(struct entropy_store *r, size_t nbytes, int min,
 	spin_lock_irqsave(&r->lock, flags);
 
 	BUG_ON(r->entropy_count > r->poolinfo->POOLBITS);
-	DEBUG_ENT("trying to extract %d bits from %s\n",
+	DEBUG_ENT("trying to extract %zu bits from %s\n",
 		  nbytes * 8, r->name);
 
 	/* Can we pull enough? */
@@ -890,7 +890,7 @@ retry:
 			wakeup_write = 1;
 	}
 
-	DEBUG_ENT("debiting %d entropy credits from %s%s\n",
+	DEBUG_ENT("debiting %zu entropy credits from %s%s\n",
 		  nbytes * 8, r->name, r->limit ? "" : " (unlimited)");
 
 	spin_unlock_irqrestore(&r->lock, flags);
