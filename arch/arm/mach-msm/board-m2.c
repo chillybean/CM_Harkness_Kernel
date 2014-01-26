1da6afe1b1822bfcdefd6a10f0483a66e6675e3d
Luis Cruz <ljc2491@gmail.com>
2014-01-26 00:51:45 -0600

    M2: Tweak GPIO Key sensitivity
    Sick of accidentally long-pressing to skip, when I mean to turn volume up on CM.
    :(
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- arch/arm/mach-msm/board-m2.c
+++ arch/arm/mach-msm/board-m2.c
@@ -4122,7 +4122,7 @@ static struct gpio_keys_button gpio_keys_button[] = {
 		.gpio			= -1,
 		.active_low		= 1,
 		.wakeup			= 0,
-		.debounce_interval	= 5, /* ms */
+		.debounce_interval	= 12, /* ms */
 		.desc			= "Vol Up",
 	},
 	{
@@ -4131,7 +4131,7 @@ static struct gpio_keys_button gpio_keys_button[] = {
 		.gpio			= -1,
 		.active_low		= 1,
 		.wakeup			= 0,
-		.debounce_interval	= 5, /* ms */
+		.debounce_interval	= 12, /* ms */
 		.desc			= "Vol Down",
 	},
 	{
