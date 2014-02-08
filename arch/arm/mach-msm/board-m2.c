c48a16ad5140eb74e638de9588caa1fbebcb3310
Luis Cruz <ljc2491@gmail.com>
2014-02-08 04:49:04 -0600

    msm: board-m2: Remove check_highblock_temp usage
    On the board's init we go through check_highblock_temp. This seems more
    useful if called on during is_sec_battery_using instead.
    
    Based on work done in the VZW M2 kernel
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- arch/arm/mach-msm/board-m2.c
+++ arch/arm/mach-msm/board-m2.c
@@ -1759,13 +1759,7 @@ static struct i2c_board_info mhl_i2c_board_info[] = {
 #endif
 
 #ifdef CONFIG_BATTERY_SEC
-static int is_sec_battery_using(void)
-{
-	if (system_rev >= 0x9)
-		return 1;
-	else
-		return 0;
-}
+static int is_sec_battery_using(void);
 
 int check_battery_type(void)
 {
@@ -1802,10 +1796,15 @@ static struct platform_device sec_device_battery = {
 	.dev.platform_data = &sec_bat_pdata,
 };
 
-static void check_highblock_temp(void)
+static int is_sec_battery_using(void)
 {
 	if (system_rev < 0xd)
 		sec_bat_pdata.high_block = 600;
+
+	if (system_rev >= 0x9)
+		return 1;
+	else
+		return 0;
 }
 
 #endif /* CONFIG_BATTERY_SEC */
@@ -5312,9 +5311,6 @@ static void __init samsung_m2_init(void)
 #ifndef CONFIG_S5C73M3
 	spi_register_board_info(spi_board_info, ARRAY_SIZE(spi_board_info));
 #endif
-#ifdef CONFIG_BATTERY_SEC
-	check_highblock_temp();
-#endif /*CONFIG_BATTERY_SEC*/
 	msm8960_init_pmic();
 	msm8960_i2c_init();
 	msm8960_gfx_init();
